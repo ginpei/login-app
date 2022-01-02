e77c2e86fba7d58ce70d36312536e512833c1d61
ginpei <ginpei@gmail.com>
2022-01-02 06:45:22 +0000

    feat: 🎸 set up Firebase and login

---
+++ packages/app/src/misc/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, connectAuthEmulator } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+const emulating = process.env.NODE_ENV !== "production";
+if (emulating) {
+  // eslint-disable-next-line no-console
+  console.log("app/src/middleware/firebase.ts: using emulators");
+  initializeEmulators();
+}
+
+function initializeEmulators() {
+  connectAuthEmulator(auth, "http://127.0.0.1:9099", { disableWarnings: true });
+}
--- packages/app/src/screens/login/LoginPage.tsx
+++ packages/app/src/screens/login/LoginPage.tsx
@@ -1,18 +1,70 @@
-import { Link } from 'react-router-dom';
-import { homePagePath } from '../home/homePageMeta';
-import { BasicLayout, NiceHeading, VStack } from '@login-app/ui';
+import { BasicLayout, NiceButton, NiceHeading, VStack } from "@login-app/ui";
+import { EmailAuthProvider, GoogleAuthProvider, signOut } from "firebase/auth";
+import { useEffect, useMemo, useState } from "react";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from '../../misc/firebase';
+import { homePagePath } from "../home/homePageMeta";
 
-export interface LoginPageProps {
-}
+const uiConfigBase: firebaseui.auth.Config = {
+  signInFlow: "popup",
+  signInOptions: [
+    GoogleAuthProvider.PROVIDER_ID,
+    EmailAuthProvider.PROVIDER_ID,
+  ],
+};
 
-export const LoginPage: React.FC<LoginPageProps> = (props) => {
+export const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+  const [loggingIn, setLoggingIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(false);
+  // const currentUser = useCurrentUser(auth);
+  const [currentUser, setCurrentUser] = useState(auth.currentUser);
+
+  const uiConfig: firebaseui.auth.Config = useMemo(() => {
+    return {
+      ...uiConfigBase,
+      callbacks: {
+        signInSuccessWithAuthResult() {
+          setLoggedIn(true);
+          navigate(homePagePath());
+          return false;
+        },
+      },
+    };
+  }, []);
+
+  useEffect(() => {
+    return auth.onAuthStateChanged(setCurrentUser);
+  }, []);
+
+  const onLogoutClick = async () => {
+    setLoggingIn(true);
+    await signOut(auth);
+    setLoggingIn(false);
+  };
+
+  if (currentUser === undefined || loggedIn) {
+    return null;
+  }
+
   return (
-    <BasicLayout title="Login">
+    <BasicLayout title="LoginPage">
       <VStack>
-        <NiceHeading>Login</NiceHeading>
+        <NiceHeading>LoginPage</NiceHeading>
         <p>
           <Link to={homePagePath()}>Home</Link>
         </p>
+        <p>User ID: {currentUser?.uid ?? "(not logged in)"}</p>
+        {currentUser ? (
+          <p>
+            <NiceButton onClick={onLogoutClick} disabled={loggingIn}>
+              {loggingIn ? "Logging out..." : "Logout"}
+            </NiceButton>
+          </p>
+        ) : (
+          <StyledFirebaseAuth firebaseAuth={auth} uiConfig={uiConfig} />
+        )}
       </VStack>
     </BasicLayout>
   );
