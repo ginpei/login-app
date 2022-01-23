4cca6a834a31bb1869cf9a861b449caa671e3187
ginpei <ginpei@ginpei.dev>
2022-01-22 19:46:37 -0800

    style: 💄 remove unused code

--- packages/app/src/pages/noteView/NoteViewPage.tsx
+++ packages/app/src/pages/noteView/NoteViewPage.tsx
@@ -1,13 +1,6 @@
-import {
-  ErrorBox,
-  LineClamp,
-  LoadingScreen,
-  NiceHeading,
-  VStack,
-} from "@login-app/ui";
+import { ErrorBox, LoadingScreen, NiceHeading, VStack } from "@login-app/ui";
 import { Link, useParams } from "react-router-dom";
 import { useLoginUser } from "../../data/LoginUserContext";
-import { Note } from "../../data/Note";
 import { useNote } from "../../data/noteHooks";
 import { AppBasicLayout } from "../../screens/appBasicLayout/AppBasicLayout";
 import { noteEditPagePath } from "../noteEdit/noteEditPageMeta";
@@ -59,34 +52,5 @@ export const NoteViewPage: React.VFC = () => {
         <p>{note.body || <small>(Empty)</small>}</p>
       </VStack>
     </AppBasicLayout>
-  );
-};
-
-const NoteList: React.VFC<{ notes: Note[] }> = ({ notes }) => {
-  if (notes.length < 1) {
-    return (
-      <p>
-        <small>(No items)</small>
-      </p>
-    );
-  }
-
-  return (
-    <VStack className="NoteList">
-      {notes.map((note) => (
-        <NoteListItem key={note.id} note={note} />
-      ))}
-    </VStack>
-  );
-};
-
-const NoteListItem: React.VFC<{ note: Note }> = ({ note }) => {
-  return (
-    <div>
-      <p>{note.title}</p>
-      <LineClamp lines={3}>
-        <small>{note.body}</small>
-      </LineClamp>
-    </div>
   );
 };
