7cd4c98426a275411bc58757cb0ebecdfd075e0e
ginpei <ginpei@ginpei.dev>
2022-01-22 17:05:38 -0800

    feat: 🎸 data record

---
+++ packages/firebase-utils/src/firestore/DataRecord.ts
@@ -0,0 +1,15 @@
+export interface DataRecord {
+  createdAt: number;
+  id: string;
+  updatedAt: number;
+}
+
+export function createDataRecord(
+  initial: Partial<DataRecord> = {}
+): DataRecord {
+  return {
+    createdAt: initial.createdAt ?? 0,
+    id: initial.id ?? "",
+    updatedAt: initial.updatedAt ?? 0,
+  };
+}
