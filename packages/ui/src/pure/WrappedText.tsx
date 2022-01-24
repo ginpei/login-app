c6ead98dc9d672e7aa99ece894dfec67391a8849
ginpei <ginpei@ginpei.dev>
2022-01-24 12:17:41 -0800

    feat: 🎸 wrap too-long words

--- packages/ui/src/pure/WrappedText.tsx
+++ packages/ui/src/pure/WrappedText.tsx
@@ -3,5 +3,9 @@ import React, { ComponentPropsWithRef } from "react";
 export type WrappedTextProps = ComponentPropsWithRef<"div">;
 
 export const WrappedText: React.FC<WrappedTextProps> = ({ children }) => {
-  return <div className="WrappedText whitespace-pre-wrap">{children}</div>;
+  return (
+    <div className="WrappedText whitespace-pre-wrap break-words">
+      {children}
+    </div>
+  );
 };
