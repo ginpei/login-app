59145d4a127e48f5e6b17b5bf8a89ea0345c469d
ginpei <ginpei@ginpei.dev>
2022-02-13 21:18:38 -0800

    feat: 🎸 radio group

---
+++ packages/ui-react/src/composite/RadioGroup.stories.tsx
@@ -0,0 +1,64 @@
+import React, { ChangeEventHandler, useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { RadioGroup } from "./RadioGroup";
+
+export default {
+  title: "Composite/RadioGroup",
+  component: RadioGroup,
+} as ComponentMeta<typeof RadioGroup>;
+
+export const Basic: ComponentStory<typeof RadioGroup> = (props) => {
+  const [selected, setSelected] = useState(props.checked);
+  const onSelectChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const { value } = event.currentTarget;
+    setSelected(value);
+  };
+
+  return (
+    <>
+      <RadioGroup {...props} checked={selected} onChange={onSelectChange} />
+      <p>
+        <small>Selected: {selected}</small>
+      </p>
+    </>
+  );
+};
+
+Basic.args = {
+  checked: "item-1",
+  name: "example",
+  options: [
+    { label: "Item 1", value: "item-1" },
+    { label: "Item 2", value: "item-2" },
+    { disabled: true, label: "Item 3", value: "item-3" },
+  ],
+};
+
+export const CustomWrappers: ComponentStory<typeof RadioGroup> = (props) => (
+  <RadioGroup {...props} />
+);
+
+CustomWrappers.args = {
+  name: "example",
+  options: [
+    { label: "Item 1", value: "item-1" },
+    { label: "Item 2", value: "item-2" },
+    { label: "Item 3", value: "item-3" },
+  ],
+  itemWrapper: ({ children }) => (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "3rem auto 3rem",
+        margin: "4px",
+      }}
+    >
+      🍣
+      {children}
+      🍡
+    </div>
+  ),
+  listWrapper: ({ children }) => (
+    <div style={{ border: "4px solid skyblue" }}>{children}</div>
+  ),
+};
---
+++ packages/ui-react/src/composite/RadioGroup.tsx
@@ -0,0 +1,59 @@
+import { ComponentPropsWithRef, ReactNode } from "react";
+import { NiceRadio } from "../primitive/NiceRadio";
+import { HStack } from "../pure/HStack";
+
+export type ItemWrapper = (props: { children: ReactNode }) => JSX.Element;
+
+type BasicInputProps = Omit<
+  ComponentPropsWithRef<"input">,
+  "checked" | "label" | "type" | "value"
+>;
+
+export interface RadioGroupProps<T extends string = string>
+  extends BasicInputProps {
+  checked: T;
+  itemWrapper?: ItemWrapper;
+  listWrapper?: ItemWrapper;
+  name: NonNullable<ComponentPropsWithRef<"input">["name"]>;
+  options: (BasicInputProps & {
+    label: string;
+    value: T;
+  })[];
+}
+
+export const RadioGroup: React.FC<RadioGroupProps> = ({
+  checked,
+  itemWrapper,
+  listWrapper,
+  options,
+  ...inputProps
+}) => {
+  const ListWrapper = listWrapper ?? DefaultListWrapper;
+  const ItemWrapper = itemWrapper ?? DefaultItemWrapper;
+
+  return (
+    <div className="RadioGroup">
+      <ListWrapper>
+        {options.map(({ label, value, ...optionProps }) => (
+          <ItemWrapper>
+            <NiceRadio
+              {...inputProps}
+              {...optionProps}
+              checked={checked === value}
+              label={label}
+              value={value}
+            />
+          </ItemWrapper>
+        ))}
+      </ListWrapper>
+    </div>
+  );
+};
+
+function DefaultListWrapper(props: { children: ReactNode }): JSX.Element {
+  return <HStack>{props.children}</HStack>;
+}
+
+function DefaultItemWrapper(props: { children: ReactNode }): JSX.Element {
+  return <>{props.children}</>;
+}
